7eeb40d64e4b0bf4432de0ff6716a81966dbcc01
Mohsen Taleb <mohsentaleb@gmail.com>
2019-11-11 22:44:52 +0330

    Moved test modules to devDependencies in package.json. Cleaned up test codes.

--- __tests__/components/board.test.js
+++ __tests__/components/board.test.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Board from '../../src/components/board';
 
-describe('Testing Board Component...', () => {
-    
-    test('Creating a Board', () => {
-        const board = shallow(<Board />);
+describe('<Board />', () => {
+    test('Board renders correctly.', () => {
+        shallow(<Board />);
     });
-    
 });
